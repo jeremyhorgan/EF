b6880cd0888051ae971d1faf48da226553cea4dc
Jeremy Horgan <jeremyhorgan@hotmail.com>
2018-11-11 22:00:00 +0000

    Initial version

---
+++ TestCollectionEncapsulation/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace TestCollectionEncapsulation
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            using (var context = new SchoolDbContext())
+            {
+                foreach (var grade in context.Grades.Include("Students"))
+                {
+                    Console.WriteLine(grade.Name);
+                    if (grade.Students != null)
+                    {
+                        foreach (var student in grade.Students)
+                        {
+                            Console.WriteLine($" {student.Name}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    internal class Grade
+    {
+        // https://blog.oneunicorn.com/2016/10/28/collection-navigation-properties-and-fields-in-ef-core-1-1/
+        private readonly ObservableCollection<Student> _students = new ObservableCollection<Student>();
+
+        public Grade()
+        {
+            _students.CollectionChanged += (sender, args) => { Console.WriteLine("CollectionChanged"); };
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public ObservableCollection<Student> Students
+        {
+            get { return _students; }
+            // set { _students = value; }
+        }
+    }
+
+    internal class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int GradeId { get; set; }
+
+        [ForeignKey(nameof(GradeId))]
+        public Grade Grade { get; set; }
+    }
+
+    internal class SchoolDbInitializer : DropCreateDatabaseAlways<SchoolDbContext>
+    {
+        protected override void Seed(SchoolDbContext context)
+        {
+            var grades = new List<Grade>
+            {
+                new Grade {Name = "A"},
+                new Grade {Name = "B"},
+                new Grade {Name = "C"},
+                new Grade {Name = "D"},
+                new Grade {Name = "E"}
+            };
+
+            var students = new List<Student>
+            {
+                new Student {Name = "Jeremy", Grade = grades[0]},
+                new Student {Name = "Bill", Grade = grades[0]},
+                new Student {Name = "Jeremy2", Grade = grades[1]},
+                new Student {Name = "Jeremy3", Grade = grades[0]}
+            };
+
+            context.Grades.AddRange(grades);
+            context.Students.AddRange(students);
+
+            base.Seed(context);
+        }
+    }
+
+    internal class SchoolDbContext : DbContext
+    {
+        private static readonly string ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\Temp\TestEfConsoleApp\Database.mdf;Integrated Security=True;Connect Timeout=30";
+
+        public SchoolDbContext() : base(ConnectionString)
+        {
+            Database.SetInitializer(new SchoolDbInitializer());
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+        public DbSet<Grade> Grades { get; set; }
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ TestCurrency/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Diagnostics;
+using System.Linq;
+
+namespace TestCurrency
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            using (var context = new SchoolDbContext())
+            {
+                context.Database.Log = Console.Write;
+
+                var chemistryCourse = context.Courses.FirstOrDefault(c => c.Title == "Chemistry");
+                Debug.Assert(chemistryCourse != null, nameof(chemistryCourse) + " != null");
+
+                chemistryCourse.Credits = 4;
+                context.Entry(chemistryCourse).OriginalValues["RowVersion"] = chemistryCourse.RowVersion;
+
+                var result = context.SaveChanges();
+
+                Console.WriteLine(result);
+            }
+
+            using (var context = new SchoolDbContext())
+            {
+                context.Database.Log = Console.Write;
+
+                var chemistryCourse = context.Courses.FirstOrDefault(c => c.Title == "Chemistry");
+                Debug.Assert(chemistryCourse != null, nameof(chemistryCourse) + " != null");
+
+                Console.WriteLine($"{chemistryCourse.Title} Credits: {chemistryCourse.Credits}");
+            }
+        }
+    }
+
+    internal class SchoolDbContext : DbContext
+    {
+        private static readonly string ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\Work\GitHub\EF\TestCurrency.mdf;Integrated Security=True;Connect Timeout=30";
+
+        public SchoolDbContext() : base(ConnectionString)
+        {
+            // Database.SetInitializer(new SchoolDbInitializer());
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+    }
+
+    internal class SchoolDbInitializer : DropCreateDatabaseAlways<SchoolDbContext>
+    {
+        protected override void Seed(SchoolDbContext context)
+        {
+            var departments = new List<Department>
+            {
+                new Department {Name = "Engineering"},
+                new Department {Name = "English"},
+                new Department {Name = "Economics"},
+                new Department {Name = "Mathematics"}
+            };
+            var courses = new List<Course>
+            {
+                new Course {Title = "Chemistry", Credits = 4, Department = departments[0]},
+                new Course {Title = "Physics", Credits = 4, Department = departments[0]},
+                new Course {Title = "Calculus", Credits = 4, Department = departments[3]},
+                new Course {Title = "Poetry", Credits = 2, Department = departments[1]},
+                new Course {Title = "Composition", Credits = 3, Department = departments[1]},
+                new Course {Title = "Literature", Credits = 4, Department = departments[1]},
+                new Course {Title = "Microeconomics", Credits = 3, Department = departments[2]},
+                new Course {Title = "Macroeconomics", Credits = 3, Department = departments[2]},
+                new Course {Title = "Quantitative", Credits = 2, Department = departments[3]},
+                new Course {Title = "Trigonometry", Credits = 4, Department = departments[3]}
+            };
+
+            context.Departments.AddRange(departments);
+            context.Courses.AddRange(courses);
+
+            base.Seed(context);
+        }
+    }
+
+    internal class Department
+    {
+        public int Id { get; set; }
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
+        public string Name { get; set; }
+        public ICollection<Course> Courses { get; set; }
+    }
+
+    internal class Course
+    {
+        public int Id { get; set; }
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+        public int DepartmentId { get; set; }
+
+        [ForeignKey(nameof(DepartmentId))]
+        public Department Department { get; set; }
+    }
+}
