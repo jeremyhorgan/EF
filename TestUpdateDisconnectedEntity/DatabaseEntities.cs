6e2a77700646189017e3e53d97434ead163e78d8
jhorgan <jhorgan@lanner.com>
2018-11-23 18:58:54 +0000

    Added new project

---
+++ TestUpdateDisconnectedEntity/DatabaseEntities.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+
+// ReSharper disable InconsistentNaming
+namespace TestUpdateDisconnectedEntity
+{
+    internal class BusinessPartner
+    {
+        public int Id { get; set; }
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime ModificationDate { get; set; }
+        public string ShortName { get; set; }
+        public string Name { get; set; }
+        public long SAPId { get; set; }
+        public int ColourCodeArgb { get; set; }
+        public String RegionalGrouping { get; set; }
+        public bool IsCustomer { get; set; }
+        public bool IsFabricator { get; set; }
+        public bool IsDeconverter { get; set; }
+        public bool IsConverter { get; set; }
+        public List<Contract> Contracts { get; set; }
+    }
+
+    internal class Contract
+    {
+        public int Id { get; set; }
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime ModificationDate { get; set; }
+        public int BusinessPartnerId { get; set; }
+        [ForeignKey(nameof(BusinessPartnerId))]
+        public BusinessPartner BusinessPartner { get; set; }
+        public string ContractNumber { get; set; }
+        public double WeightTolerance { get; set; }
+        public string ProductContainerSupply { get; set; }
+        public int FinalNotificationPeriod { get; set; }
+        public int ProvisionalNotificationPeriod { get; set; }
+        public int ContainerDeliveryPeriod { get; set; }
+        public double MinimumAssayTolerance { get; set; }
+        public double AssayTolerance { get; set; }
+        public int CollectionDateOffset { get; set; }
+        public int TransportAllowancePeriod { get; set; }
+        public int ArbitrationPeriod { get; set; }
+        public string DeliveryContainerOwnership { get; set; }
+        public int UrencoBusinessEntityId { get; set; }
+        [ForeignKey(nameof(UrencoBusinessEntityId))]
+        public UrencoBusinessEntity UrencoBusinessEntity { get; set; }
+        public List<ContractSWUOriginPermitted> SWUOriginsPermitted { get; set; }
+        public List<ContractCylinderOwnershipPermitted> CylinderOwnershipsPermitted { get; set; }
+    }
+
+    internal class ContractSWUOriginPermitted
+    {
+        public int Id { get; set; }
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime ModificationDate { get; set; }
+        public int ContractId { get; set; }
+        [ForeignKey(nameof(ContractId))]
+        public Contract Contract { get; set; }
+        public int SwuOriginId { get; set; }
+        [ForeignKey(nameof(SwuOriginId))]
+        public SWUOrigin SwuOrigin { get; set; }
+    }
+
+    internal class ContractCylinderOwnershipPermitted
+    {
+        public int Id { get; set; }
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime ModificationDate { get; set; }
+        public int ContractId { get; set; }
+        [ForeignKey(nameof(ContractId))]
+        public Contract Contract { get; set; }
+        public int BusinessPartnerId { get; set; }
+        [ForeignKey(nameof(BusinessPartnerId))]
+        public BusinessPartner BusinessPartner { get; set; }
+
+    }
+
+    internal class UrencoBusinessEntity
+    {
+        public int Id { get; set; }
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime ModificationDate { get; set; }
+        public string BusinessEntityName { get; set; }
+        public string UranicsPlantName { get; set; }
+    }
+
+    internal class SWUOrigin
+    {
+        public int Id { get; set; }
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime ModificationDate { get; set; }
+        public string Code { get; set; }
+        public string Description { get; set; }
+        public int Rank { get; set; }
+        public int ColourCode { get; set; }
+    }
+
+    internal class AppDbContext : DbContext
+    {
+        private static readonly string ConnectionString = @"Data Source=(local)\SQLEXPRESS;Database=DPS2;Trusted_Connection=True";
+
+        public AppDbContext() : base(ConnectionString)
+        {
+        }
+
+        public DbSet<UrencoBusinessEntity> UrencoBusinessEntities { get; set; }
+        public DbSet<SWUOrigin> SWUOrigins { get; set; }
+        public DbSet<BusinessPartner> BusinessPartners { get; set; }
+        public DbSet<Contract> Contracts { get; set; }
+        public DbSet<ContractSWUOriginPermitted> ContractSWUOriginPermitteds { get; set; }
+        public DbSet<ContractCylinderOwnershipPermitted> ContractCylinderOwnershipPermitteds { get; set; }
+    }
+}
---
+++ TestUpdateDisconnectedEntity/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+// ReSharper disable InconsistentNaming
+
+namespace TestUpdateDisconnectedEntity
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var businessPartner = FetchBusinessPartner();
+            DisplayBusinessPartner(businessPartner);
+
+            var contracts = FetchContracts(businessPartner);
+            var contract = contracts.First();
+            DisplayContract(contract);
+
+            AddContractSWOriginPermitted(contract);
+            // AddNewContractWithSWOriginPermitted(businessPartner);
+        }
+
+        private static void DisplayBusinessPartner(BusinessPartner businessPartner)
+        {
+            Console.WriteLine($"Business Partner:{businessPartner.Id} {businessPartner.Name}");
+        }
+
+        private static void DisplayContract(Contract contract)
+        {
+            Console.WriteLine($"Business Partner:{contract.BusinessPartner.Id} {contract.BusinessPartner.Name}");
+            Console.WriteLine($"Contract No:                {contract.Id} {contract.ContractNumber}");
+            Console.WriteLine($"SWUOriginsPermitted:            {contract.SWUOriginsPermitted.Count}");
+            Console.WriteLine($"CylinderOwnershipsPermitted:    {contract.CylinderOwnershipsPermitted.Count}");
+        }
+
+        private static BusinessPartner FetchBusinessPartner()
+        {
+            using (var context = new AppDbContext())
+            {
+                return context.BusinessPartners.FirstOrDefault();
+            }
+        }
+
+        private static IEnumerable<Contract> FetchContracts(BusinessPartner businessPartner)
+        {
+            using (var context = new AppDbContext())
+            {
+                return context.Contracts
+                    .Include(x => x.UrencoBusinessEntity)
+                    .Include(x => x.BusinessPartner)
+                    .Include(x => x.SWUOriginsPermitted)
+                    .Include(x => x.SWUOriginsPermitted.Select(s => s.SwuOrigin))
+                    .Include(x => x.CylinderOwnershipsPermitted)                    
+                    .ToList();
+            }
+        }
+
+        private static List<SWUOrigin> FetchSWUOrigins()
+        {
+            using (var context = new AppDbContext())
+            {
+                return context.SWUOrigins.ToList();
+            }
+        }
+
+        private static void AddContractSWOriginPermitted(Contract contract)
+        {
+            var swOrigins = FetchSWUOrigins();
+            contract.ContractNumber = "12345";
+            var contractSWUOriginPermitted = new ContractSWUOriginPermitted
+            {
+                // ContractId = contract.Id,
+                Contract = contract,
+                ModificationDate = DateTime.Now,
+                ModifiedBy = "jhorgan",     
+                SwuOrigin = swOrigins[1],
+                // SwuOriginId = swOrigins[1].Id
+            };
+
+            contract.SWUOriginsPermitted.Add(contractSWUOriginPermitted);
+
+            using (var context = new AppDbContext())
+            {
+                context.Contracts.Attach(contract);
+                context.Entry(contract).State = EntityState.Modified;
+                context.Entry(contract.BusinessPartner).State = EntityState.Unchanged;
+                context.Entry(contract.UrencoBusinessEntity).State = EntityState.Unchanged;
+
+                foreach (var swuOriginsPermitted in contract.SWUOriginsPermitted)
+                {
+                    var state = swuOriginsPermitted.Id == 0 ? EntityState.Added : EntityState.Unchanged;
+                    context.Entry(swuOriginsPermitted).State = state;
+
+                    // context.Entry(contractSWUOriginPermitted.SwuOrigin).State = EntityState.Unchanged;
+                }
+
+                context.SaveChanges();
+           }
+        }
+
+        private static void AddNewContractWithSWOriginPermitted(BusinessPartner businessPartner)
+        {
+            var swOrigins = FetchSWUOrigins();
+
+            var contract = new Contract
+            {
+                ContractNumber = "1234567",
+                ProductContainerSupply = "1234567",
+                DeliveryContainerOwnership = "1234567",
+                BusinessPartner = businessPartner,
+                BusinessPartnerId = businessPartner.Id,
+                UrencoBusinessEntityId = 1,
+                ModificationDate = DateTime.Now,
+                ModifiedBy = "jhorgan"
+            };
+
+            var contractSWUOriginPermitted = new ContractSWUOriginPermitted
+            {
+                ModificationDate = DateTime.Now,
+                ModifiedBy = "jhorgan",
+                SwuOrigin = swOrigins[1],
+                SwuOriginId = swOrigins[1].Id
+            };
+
+            contract.SWUOriginsPermitted = new List<ContractSWUOriginPermitted> {contractSWUOriginPermitted};
+
+            using (var context = new AppDbContext())
+            {
+                context.Entry(contract).State = EntityState.Added;
+                context.Entry(contract.BusinessPartner).State = EntityState.Unchanged;
+                
+                foreach (var swuOriginsPermitted in contract.SWUOriginsPermitted)
+                {
+                    var state = swuOriginsPermitted.Id == 0 ? EntityState.Added : EntityState.Unchanged;
+                    context.Entry(swuOriginsPermitted).State = state;
+
+                    context.Entry(contractSWUOriginPermitted.SwuOrigin).State = EntityState.Unchanged;
+                }
+
+                context.SaveChanges();
+            }
+
+        }
+    }
+}
